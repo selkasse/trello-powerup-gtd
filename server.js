55fab6e7f7d7981fe340458b80a48420c321f757
selkasse <selkasse@gmu.edu>
2020-05-05 07:26:15 -0400

    added remaning config to server.js, created process.env and added port number

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({ origin: 'https://trello.com' }));
+
+app.use(express.static('_site'));
+
+app.get("*", function(request, response){
+    response.sendFile(__dirname + 'index.html');
+});
+
+let listener = app.listen(process.env.PORT, function() {
+    console.log(`Your powerup is listening on port ${listener.address().port}`);
+})
