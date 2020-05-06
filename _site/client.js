0a359e16ba5e520ccbd5fa18c0b5fb53caeb1e2e
selkasse <selkasse@gmu.edu>
2020-05-06 18:31:33 -0400

    console logging t.board. oops

--- _site/client.js
+++ _site/client.js
@@ -12,7 +12,8 @@ const onCardBtnClick = function (t, options) {
 }
 
 const onBoardBtnClick = function(t, options){
-    console.log(t.card);
+    // try t["board"] if this does not work
+    console.log(t.board);
     return t.popup({
         title: 'Change Master Board',
         url: 'master.html'
