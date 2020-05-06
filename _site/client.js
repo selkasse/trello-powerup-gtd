d4996f799957c1541680ebbe6ac69aa2918e17b2
selkasse <selkasse@gmu.edu>
2020-05-06 19:55:18 -0400

    logging response without parsing JSON, as it returned an error with JSON parse

--- _site/client.js
+++ _site/client.js
@@ -32,12 +32,7 @@ fetch(`https://api.trello.com/1/members/${userId}/boards?key=41ae5bff41af5eac3f3
         'Accept': 'application/json'
     }
 })
-    .then(response => {
-        console.log(
-            `Response: ${response.status} ${response.statusText}`
-        );
-        return response.text();
-    })
+    .then(text => console.log(text))
     .then(text => {
         const res = (JSON.parse(text));
         console.log(res);
