1b695e7353df7a506d5af237c9cdfe227bc07e56
selkasse <selkasse@gmu.edu>
2020-05-05 20:44:13 -0400

    add comments to schedule.js

--- _site/schedule.js
+++ _site/schedule.js
@@ -3,12 +3,14 @@ const t = TrelloPowerUp.iframe();
 window.schedule.addEventListener('submit', function (event) {
     // Stop the browser trying to submit the form itself.
     event.preventDefault();
+    // Set the scheduled date for the card
     return t.set('card', 'shared', 'schedule', window.scheduledCard.value)
         .then(function () {
             t.closePopup();
         });
 });
 
+// When the card button is pressed, display the current value of the scheduled date
 t.render(function () {
     return t.get('card', 'shared', 'schedule')
         .then(function (scheduledDate) {
