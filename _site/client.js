d21c90dd7c8f1416d214e236d9dd3817eda1bf35
selkasse <selkasse@gmu.edu>
2020-05-06 20:45:40 -0400

    add await keyword in front of call to .json()

--- _site/client.js
+++ _site/client.js
@@ -13,7 +13,7 @@ async function getMemberId() {
     const memberId = memberResponse.id;
 
     let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
-    let boardsJSON = boardsResponse.json();
+    let boardsJSON = await boardsResponse.json();
     console.log(boardsJSON);
 
     // fetch('https://api.trello.com/1/members/me?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661', {
