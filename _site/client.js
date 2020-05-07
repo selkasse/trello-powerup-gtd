aeb18ec0054f56f1bae527cc22f0a25c8473349e
selkasse <selkasse@gmu.edu>
2020-05-06 20:52:35 -0400

    add await to call to getMemberId in getMemberBoards()'

--- _site/client.js
+++ _site/client.js
@@ -16,7 +16,7 @@ async function getMemberId() {
 
 // get the boards that belong to the user
 async function getMemberBoards(){
-    const memberId = getMemberId();
+    const memberId = await getMemberId();
     
     let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
     let boardsJSON = await boardsResponse.json();
