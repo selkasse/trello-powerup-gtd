bd3cb4cfc9589fcb1e22d2b9090103bd29b038e2
selkasse <selkasse@gmu.edu>
2020-05-06 21:16:10 -0400

    add master.js and link to it in master.html.  move getMemberID and getMembrerBoards methods from server.js to master.js

--- _site/client.js
+++ _site/client.js
@@ -4,29 +4,6 @@ const CHECK_MARK_ICON = 'https://img.icons8.com/material/24/000000/check-all.png
 const MASTER_ICON_DARK = 'https://img.icons8.com/material/24/000000/master.png';
 const MASTER_ICON_LIGHT = 'https://img.icons8.com/material-outlined/24/000000/master.png';
 
-
-// get the member (user) ID
-async function getMemberId() {
-
-    let response = await fetch('https://api.trello.com/1/members/me?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661');
-    let memberResponse = await response.json();
-    return memberResponse.id;
-
-}
-
-// get the boards that belong to the user
-async function getMemberBoards(){
-    const memberId = await getMemberId();
-    
-    let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
-    let boardsJSON = await boardsResponse.json();
-    const boardNames = boardsJSON.map(board => board.name)
-    console.log(boardNames);
-    
-}
-
-getMemberBoards();
-
 const onCardBtnClick = function (t, options) {
     return t.popup({
         title: 'Add to future board',
---
+++ _site/master.js
@@ -0,0 +1,21 @@
+// get the member (user) ID
+async function getMemberId() {
+
+    let response = await fetch('https://api.trello.com/1/members/me?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661');
+    let memberResponse = await response.json();
+    return memberResponse.id;
+
+}
+
+// get the boards that belong to the user
+async function getMemberBoards() {
+    const memberId = await getMemberId();
+
+    let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
+    let boardsJSON = await boardsResponse.json();
+    const boardNames = boardsJSON.map(board => board.name)
+    console.log(boardNames);
+
+}
+
+getMemberBoards();
