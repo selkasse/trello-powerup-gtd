fd0a0b23a58bd9dc127ad3167be1fa817d3a11d7
selkasse <selkasse@gmu.edu>
2020-05-06 20:42:54 -0400

    added another await fetch within getMemberId in attempts to retrive the boards

--- _site/client.js
+++ _site/client.js
@@ -11,7 +11,11 @@ async function getMemberId() {
     let response = await fetch('https://api.trello.com/1/members/me?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661');
     let memberResponse = await response.json();
     const memberId = memberResponse.id;
-    console.log(memberId);
+
+    let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
+    let boardsJSON = boardsResponse.json();
+    console.log(boardsJSON);
+
     // fetch('https://api.trello.com/1/members/me?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661', {
     //     method: 'GET',
     //     headers: {
