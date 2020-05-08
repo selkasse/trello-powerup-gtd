5270c2e82255524716917930975f3f83c58c28c2
selkasse <selkasse@gmu.edu>
2020-05-07 22:07:09 -0400

    refactor getMemberBoards so that it returns an array of objects, which contains key value pairs for the board id and name

--- _site/master.js
+++ _site/master.js
@@ -15,13 +15,21 @@ async function getMemberBoards() {
 
     let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
     let boardsJSON = await boardsResponse.json();
-    return boardsJSON;
+    // return boardsJSON;
+    let boards = [];
+    for(board in boardsJSON){
+        const obj = {
+            id: board.id,
+            name: board.name
+        };
+        boards.push(obj);
+    
+    }
+    return boards;
 }
 
-function getBoardNames(boardsJSON){
-    const boardNames = boardsJSON.map(board => board.name)
-    return boardNames;
-}
+// function getBoardNames(boardsJSON){
+// }
 
 
 // return boards JSON from getMemberBoards
@@ -31,11 +39,11 @@ function getBoardNames(boardsJSON){
 // add the boards to the dropdown when master.html renders
 t.render(async function(){
     const boards = await getMemberBoards();
-    const boardNames = getBoardNames(boards);
+    // const boardNames = getBoardNames(boards);
     const select = document.getElementById("masterBoard");
-    for (board in boardNames){
+    for (board in boards){
         const option = document.createElement("option");
-        option.text = boardNames[board];
+        option.text = boards[board].name;
         select.add(option);
     }
     t.sizeTo('#master').done(); 
