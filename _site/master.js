9e932d567e218ac27b1a780d22bca3b54b5ebff0
selkasse <selkasse@gmu.edu>
2020-05-07 22:15:57 -0400

    logging each board in for loop within getMemberBoards

--- _site/master.js
+++ _site/master.js
@@ -15,10 +15,11 @@ async function getMemberBoards() {
 
     let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
     let boardsJSON = await boardsResponse.json();
-    console.log(boardsJSON);
+    // console.log(boardsJSON);
     // return boardsJSON;
     let boards = [];
     for(board in boardsJSON){
+        console.log(board);
         const obj = {
             id: board.id,
             name: board.name
@@ -40,10 +41,12 @@ async function getMemberBoards() {
 // add the boards to the dropdown when master.html renders
 t.render(async function(){
     const boards = await getMemberBoards();
-    console.log(boards);
+    // console.log(boards);
     // const boardNames = getBoardNames(boards);
     const select = document.getElementById("masterBoard");
     for (board in boards){
+        // call getBoardPowerUps, passing the board ID
+        // only create an option if the Trello GTD powerup is enabled on the board
         const option = document.createElement("option");
         option.text = boards[board].name;
         select.add(option);
