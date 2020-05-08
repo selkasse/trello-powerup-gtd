b6436a6d865fa26c1c2d005b6ae72a9d5ed2d0f2
selkasse <selkasse@gmu.edu>
2020-05-08 14:56:00 -0400

    remove second '.value'

--- _site/master.js
+++ _site/master.js
@@ -4,7 +4,7 @@ window.master.addEventListener('submit', function (event) {
     // Stop the browser trying to submit the form itself.
     event.preventDefault();
     // Set the master board
-    return t.set('member', 'shared', 'masterBoard', window.masterBoard.value.value)
+    return t.set('member', 'shared', 'masterBoard', window.masterBoard.value)
         .then(function () {
             t.closePopup();
         });
