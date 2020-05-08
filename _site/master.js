e8dd02e623be707b8e610cf07b64fa87197f9d13
selkasse <selkasse@gmu.edu>
2020-05-07 21:53:40 -0400

    refactor getMemberBoards: break out the names map into a separate function: getBoardNames. testing to see if the dropdown still renders

--- _site/master.js
+++ _site/master.js
@@ -15,18 +15,27 @@ async function getMemberBoards() {
 
     let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
     let boardsJSON = await boardsResponse.json();
+    return boardsJSON;
+}
+
+function getBoardNames(boardsJSON){
     const boardNames = boardsJSON.map(board => board.name)
     return boardNames;
 }
 
 
+// return boards JSON from getMemberBoards
+// break out boardNames into a new function: getBoardNames (not async)
+// make a new function, getBoardPowerups(board)
+
 // add the boards to the dropdown when master.html renders
 t.render(async function(){
     const boards = await getMemberBoards();
+    const boardNames = getBoardNames(boards);
     const select = document.getElementById("masterBoard");
-    for(board in boards){
+    for (board in boardNames){
         const option = document.createElement("option");
-        option.text = boards[board];
+        option.text = boardNames[board];
         select.add(option);
     }
     t.sizeTo('#master').done(); 
