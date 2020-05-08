e5e5b39a8575c96af144a20efaaad52adf3bb9d7
selkasse <selkasse@gmu.edu>
2020-05-08 07:58:32 -0400

    clean up comments in master.js

--- _site/master.js
+++ _site/master.js
@@ -15,8 +15,6 @@ async function getMemberBoards() {
 
     let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
     let boardsJSON = await boardsResponse.json();
-    // console.log(boardsJSON);
-    // return boardsJSON;
     let boards = [];
     for(board in boardsJSON){
         const obj = {
@@ -24,7 +22,6 @@ async function getMemberBoards() {
             name: boardsJSON[board].name
         };
         boards.push(obj);
-    
     }
     return boards;
 }
@@ -39,27 +36,15 @@ async function checkIfEnabled(id){
 }
 
 
-// return boards JSON from getMemberBoards
-// break out boardNames into a new function: getBoardNames (not async)
-// make a new function, getBoardPowerups(board)
-
 // add the boards to the dropdown when master.html renders
 t.render(async function(){
     const boards = await getMemberBoards();
-    // console.log(boards);
-    // const boardNames = getBoardNames(boards);
     const select = document.getElementById("masterBoard");
     for (board in boards){
-        // call getBoardPowerUps, passing the board ID
         // only create an option if the Trello GTD powerup is enabled on the board
         const powerupEnabled = await checkIfEnabled(boards[board].id);
-        // if(boards[board].name === 'GTD Powerup Test'){
 
-        //     console.log(boards[board].name);
-        //     console.log(powerupEnabled);
-        // }
         if (powerupEnabled){
-
             const option = document.createElement("option");
             option.text = boards[board].name;
             select.add(option);
@@ -68,7 +53,6 @@ t.render(async function(){
     t.sizeTo('#master').done(); 
 })
 
-// exports.getMemberBoards = getMemberBoards;
 
 
 
